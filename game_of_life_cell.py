3f2867161010a00862845d0f7260428e41ec21b2
Odd Meta <odd.meta@gmail.com>
2013-03-06 01:15:51 -0500

    added basis of cellular text display, random grid generation, and the beginning of adjacent cell calculation

--- game_of_life_cell.py
+++ game_of_life_cell.py
@@ -5,8 +5,152 @@
 import pygame
 from pygame.locals import *
 
+import random
+
 class Agent:
     pass
+
+
+class Grid:
+
+	def __init__(self, state = [], max_width = 10, max_height = 10):
+		self.state = state
+		self.max_width = max_width
+		self.max_height = max_height
+		if len(state) is 0:
+			self.state = self.generate_state()
+
+	def do_step(self):
+		new_state = []
+		for row_index, row in enumerate(self.state):
+			
+			for cell_index, cell in enumerate(row):
+				adjacent_cells = []
+				#TODO: consolidate if statements
+				#get top row
+				top_left_y = None
+				top_left_x = None
+				top_y = None
+				top_x = None
+				top_right_y = None
+				top_right_x = None
+
+				#handle top left cell
+				if cell_index - 1 < 0:
+					top_left_y = self.max_height - 1
+				else:
+					top_left_y = cell_index - 1
+
+				if row_index - 1 < 0:
+					top_left_x = self.max_width -1
+				else:
+					top_left_x = row_index - 1
+
+				#handle top cell
+				top_y = cell_index
+				if row_index - 1 < 0:
+					top_x = self.max_width - 1
+				else:
+					top_x = row_index - 1
+
+				#handle top right cell
+				if cell_index + 1 >= self.max_height:
+					top_right_y = 0
+				else:
+					top_right_y = cell_index + 1
+				if row_index + 1 >= self.max_width:
+					top_right_x = 0
+				else:
+					top_right_x = row_index + 1
+
+
+				#get sides
+
+				left_side_y = None
+				left_side_x = None
+				right_side_y = None
+				right_side_x = None
+
+				#FUCKING FUCK TEDIOUS CODE FUCK HOW DID I EVER HAND TYPE THIS INTO A TI-89 GOOD GOD
+
+				if cell_index - 1 < 0:
+					left_side_y = self.max_height - 1
+				else:
+					left_side_y = cell_index - 1
+				left_side_x = row_index
+
+				if cell_index + 1 >= self.max_height:
+					right_side_y = 0
+				else:
+					right_side_y = cell_index + 1
+
+				#get bottom row
+				bottom_left_y = None
+				bottom_left_x = None
+				bottom_y = None
+				bottom_x = None
+				bottom_right_y = None
+				bottom_right_x = None
+
+				if cell_index - 1 < 0:
+					bottom_left_y = self.max_height - 1
+				else:
+					bottom_left_y = cell_index - 1
+
+				if row_index + 1 >= self.max_width:
+					bottom_left_x = 0
+				else:
+					bottom_left_x = row_index + 1
+
+				bottom_y = cell_index
+				if row_index + 1 >= self.max_width:
+					bottom_x = 0
+				else:
+					bottom_x = row_index + 1
+
+				if cell_index + 1 >= self.max_height:
+					bottom_right_y = 0
+				else:
+					bottom_right_y = cell_index + 1
+
+				if row_index + 1 >= self.max_width:
+					bottom_right_x = 0
+				else:
+					bottom_right_x = row_index + 1
+
+				adjacent_cells = [	(top_left_x,top_left_y),
+									(top_x,top_y),
+									(top_right_x,top_right_y),
+									(left_side_x,left_side_y),
+									(right_side_x,right_side_y),
+									(bottom_left_x,bottom_left_y),
+									(bottom_x,bottom_y),
+									(bottom_right_x,bottom_right_y)]
+
+
+
+		
+
+	def generate_state(self):
+		temp_state = []
+
+		for x in range(0,self.max_height):
+			temp_row = []
+			for y in range(0,self.max_width):
+				temp_row.append( random.randint(0,1) )
+			temp_state.append(temp_row)
+		return temp_state
+
+	def __str__(self):
+		the_string = ""
+		for row in self.state:
+			the_string += str(row) + "\n"
+		return the_string
+
+
+
+
+
 
 
 class App:
@@ -66,5 +210,8 @@ class App:
  
 if __name__ == "__main__" :
 
-    theApp = App()
-    theApp.on_execute()
+    #theApp = App()
+    #theApp.on_execute()
+
+    theGrid = Grid()
+    print theGrid
