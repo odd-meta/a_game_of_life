9c3fb79d635b852c3d1360174da2b57f9f0d471a
Odd Meta <odd.meta@gmail.com>
2013-03-09 03:38:06 -0500

    put build in single file, breaks build

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ import sys
 
 sys.argv.append('py2exe')
 
-setup(windows=['game_of_life_cell.py'], 
-	data_files=[('.', ['libiomp5md.dll']),],
+setup(
+	windows=['game_of_life_cell.py'], 
+	data_files=[('.', ['libiomp5md.dll',"icon.bmp"]),],
     options = {
         'py2exe': {
+        	'bundle_files': 1,
             'includes': ['ctypes', 'logging'],
             'excludes': ['OpenGL']
         }
