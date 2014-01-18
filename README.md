aaa0718832e84a648cbfbcb5b8071a40d28ecd08
odd-meta <odd.meta@gmail.com>
2014-01-17 22:53:28 -0500

    fixed window size to a square again to unbreak program, added right click reset function, added real readme

--- README.md
+++ README.md
@@ -1,87 +1,15 @@
-# A(gent) Game of Life
+# A Game of Life
 
-The game of life using an agent model
+A simple pygame toy application that implements [Conway's game of life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life). Based off a multitude of pygame tutorials I've found and never wrote down; the functional structure of the App class mostly from [this one](http://pygametutorials.wikidot.com/tutorials-basic).
 
+## Dependancies
 
-## AGENTS
-Each live cell will be defined as an Agent
+Should only require the pygame package to be installed.
 
-Each agent keeps track of it's own position, as well as the environment
+## Use
 
-Every agent is visited in the list of agents and told to run against the defined rules based on it's environment
+Running game_of_life_cell.py should create a pygame window with a running game of life in it. Left clicking pauses the simulation, right clicking resets it.
 
-The changes are buffered until all changes are collected from all agents, then all changes are applied in the sequence they were collected
+## TODO
 
-An environment is desired to save the headache of visiting an agent, then walking down the entire list to find what is near it
-
-have the environment keep track of, agent locations
-
-in the form of
-
-[( (X, Y), Agent )]
-
-give the environment a set of coordinates and a range bounding (list of tuples containing points)
-the environement returns a list of agents that are within that range, you can simply ask each agent what their coordinates are
-
-how does the environment figure that out
-
-given a list of x,y coordinates
-
-search them for X coordinates within a +/- range, drop the rest
-then look for Y coordinates within a +/- range, this is your list to return
-a step here could be added for Z, and t, if desired
-
-
-## CHANGE BUFFER
-
-step through the agents for an area and evaluate their actions for this step based on rules
-put those actions into the first-stage change buffer
-
-evaluate the actions in the first stage change buffer for actions that will create conflicts
-process the conflicts according to the rules
-fill back into first stage buffer
-repeat
-
-this might lockup if actions and conflicts interact to create a never-ending loop of conflict resolutions
-
-
-## ENVIRONMENT
-
-a container for a given area that knows about adjacent areas
-
-coordinates in a global sense are defined as
-
-area, area-coordinates
-
-where all area-coordinates are an x by y grid, this, prefixed with an area identifier, defines a position in global space
-
-every area has a bounding fuction that will return the "edges" of the area; this is a list of lists of coordinate tuples
-
-each list of tuples defines a bounding 
-
-also know about agents within it
-
-if it is found that an agent will move to another area
-
-
-## RULES
-
-based off of the definitions in "Growing Artificial Societies"
-
-agent-agent
-* govern agent's interactions with each other
-* behavior such as mating, fighting, socializing, etc
-* the game of life is focused on mating behavior
-
-environment-environment
-* these involve properties of the environment, ambient resources, weather, etc
-* not really applicable to the classic game of life
-
-agent-environment
-* these control how the agents react to properties of their environment, eating sugar in sugarscape, for example
-* not really applicable to the classic game of life
-
-TODO
-
-* create the agent class
-** define 
+Get pyinstalled exe generation scripted and working in a self contained manner.
--- game_of_life_cell.py
+++ game_of_life_cell.py
@@ -11,6 +11,8 @@ import random
 
 #little diddy to make resources work with pyinstaller onefile mode exes
 #thanks to http://stackoverflow.com/a/7675014
+#for example: icon_surface = pygame.image.load( resource_path("assets"+os.sep+"icon.bmp") )
+
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
@@ -27,189 +29,187 @@ class Agent:
 
 class Grid:
 
-	def __init__(self, state = [], max_width = 20, max_height = 20):
-		self.state = state
-		self.max_width = max_width
-		self.max_height = max_height
-		if len(state) is 0:
-			self.state = self.generate_state()
+    def __init__(self, state = [], max_width = 20, max_height = 20):
+        self.state = state
+        self.max_width = max_width
+        self.max_height = max_height
+        if len(state) is 0:
+            self.state = self.generate_state()
 
-	def do_step(self):
-		new_state = []
+    def reset_state(self):
+        self.state = self.generate_state()
 
-		for r in range(0,self.max_width):
-			new_state.append([])
-			for c in range(0,self.max_height):
-				new_state[r].append(None)
+    def do_step(self):
+        new_state = []
 
-		for row_index, row in enumerate(self.state):
-			
-			for cell_index, cell in enumerate(row):
-				adjacent_cells = []
-				#TODO: consolidate if statements
-				#get top row
-				top_left_y = None
-				top_left_x = None
-				top_y = None
-				top_x = None
-				top_right_y = None
-				top_right_x = None
+        for r in range(0,self.max_width):
+            new_state.append([])
+            for c in range(0,self.max_height):
+                new_state[r].append(None)
 
-				#handle top left cell
-				if cell_index - 1 < 0:
-					top_left_y = self.max_height - 1
-				else:
-					top_left_y = cell_index - 1
+        for row_index, row in enumerate(self.state):
+            
+            for cell_index, cell in enumerate(row):
+                adjacent_cells = []
+                #TODO: consolidate if statements
+                #get top row
+                top_left_y = None
+                top_left_x = None
+                top_y = None
+                top_x = None
+                top_right_y = None
+                top_right_x = None
 
-				if row_index - 1 < 0:
-					top_left_x = self.max_width -1
-				else:
-					top_left_x = row_index - 1
+                #handle top left cell
+                if cell_index - 1 < 0:
+                    top_left_y = self.max_height - 1
+                else:
+                    top_left_y = cell_index - 1
 
-				#handle top cell
-				top_y = cell_index
-				if row_index - 1 < 0:
-					top_x = self.max_width - 1
-				else:
-					top_x = row_index - 1
+                if row_index - 1 < 0:
+                    top_left_x = self.max_width -1
+                else:
+                    top_left_x = row_index - 1
 
-				#handle top right cell
-				if cell_index + 1 >= self.max_height:
-					top_right_y = 0
-				else:
-					top_right_y = cell_index + 1
-				if row_index - 1 < 0:
-					top_right_x = self.max_width -1
-				else:
-					top_right_x = row_index - 1
+                #handle top cell
+                top_y = cell_index
+                if row_index - 1 < 0:
+                    top_x = self.max_width - 1
+                else:
+                    top_x = row_index - 1
 
-
-				#get sides
-
-				left_side_y = None
-				left_side_x = None
-				right_side_y = None
-				right_side_x = None
-
-				#FUCKING FUCK TEDIOUS CODE FUCK HOW DID I EVER HAND TYPE THIS INTO A TI-89 GOOD GOD
-
-				if cell_index - 1 < 0:
-					left_side_y = self.max_height - 1
-				else:
-					left_side_y = cell_index - 1
-				left_side_x = row_index
-
-				if cell_index + 1 >= self.max_height:
-					right_side_y = 0
-				else:
-					right_side_y = cell_index + 1
-				right_side_x = row_index
-
-				#get bottom row
-				bottom_left_y = None
-				bottom_left_x = None
-				bottom_y = None
-				bottom_x = None
-				bottom_right_y = None
-				bottom_right_x = None
-
-				if cell_index - 1 < 0:
-					bottom_left_y = self.max_height - 1
-				else:
-					bottom_left_y = cell_index - 1
-
-				if row_index + 1 >= self.max_width:
-					bottom_left_x = 0
-				else:
-					bottom_left_x = row_index + 1
-
-				bottom_y = cell_index
-				if row_index + 1 >= self.max_width:
-					bottom_x = 0
-				else:
-					bottom_x = row_index + 1
-
-				if cell_index + 1 >= self.max_height:
-					bottom_right_y = 0
-				else:
-					bottom_right_y = cell_index + 1
-
-				if row_index + 1 >= self.max_width:
-					bottom_right_x = 0
-				else:
-					bottom_right_x = row_index + 1
-
-				adjacent_cells = [	(top_left_x,top_left_y),
-									(top_x,top_y),
-									(top_right_x,top_right_y),
-									(left_side_x,left_side_y),
-									(right_side_x,right_side_y),
-									(bottom_left_x,bottom_left_y),
-									(bottom_x,bottom_y),
-									(bottom_right_x,bottom_right_y)]
-				#evaluate our next state based on rules
-
-				#get number of adjacent alive cells
-				our_state = int(self.state[row_index][cell_index])
-				alive_cells = 0
-
-				for cell in adjacent_cells:
-					#if our_state is 1:
-					#	print str(self.state[cell[0]][cell[1]])+" "+str( (cell[0],cell[1]) )
-					alive_cells += int(self.state[cell[0]][cell[1]])
-
-				
-				our_new_state = None
-				if our_state is 1:
-					if alive_cells > 3:
-						our_new_state = 0
-					elif alive_cells < 2:
-						our_new_state = 0
-					elif alive_cells is 2 or alive_cells is 3:
-						our_new_state = 1
-					else:
-						print "i cant even boolean logic"
-				else:
-					if alive_cells is 3:
-						our_new_state = 1
-					else:
-						our_new_state = 0
-				new_state[row_index][cell_index] = our_new_state
-				#if our_state is 1:
-				#	print alive_cells
-				#	print (row_index,cell_index)
-					
+                #handle top right cell
+                if cell_index + 1 >= self.max_height:
+                    top_right_y = 0
+                else:
+                    top_right_y = cell_index + 1
+                if row_index - 1 < 0:
+                    top_right_x = self.max_width -1
+                else:
+                    top_right_x = row_index - 1
 
 
+                #get sides
 
+                left_side_y = None
+                left_side_x = None
+                right_side_y = None
+                right_side_x = None
 
-		#print "*****"
-		self.state = new_state
+                #FUCKING FUCK TEDIOUS CODE FUCK HOW DID I EVER HAND TYPE THIS INTO A TI-89 GOOD GOD
 
+                if cell_index - 1 < 0:
+                    left_side_y = self.max_height - 1
+                else:
+                    left_side_y = cell_index - 1
+                left_side_x = row_index
 
+                if cell_index + 1 >= self.max_height:
+                    right_side_y = 0
+                else:
+                    right_side_y = cell_index + 1
+                right_side_x = row_index
 
-		
+                #get bottom row
+                bottom_left_y = None
+                bottom_left_x = None
+                bottom_y = None
+                bottom_x = None
+                bottom_right_y = None
+                bottom_right_x = None
 
-	def generate_state(self):
-		temp_state = []
+                if cell_index - 1 < 0:
+                    bottom_left_y = self.max_height - 1
+                else:
+                    bottom_left_y = cell_index - 1
 
-		for x in range(0,self.max_height):
-			temp_row = []
-			for y in range(0,self.max_width):
-				temp_row.append( random.randint(0,1) )
-			temp_state.append(temp_row)
-		return temp_state
+                if row_index + 1 >= self.max_width:
+                    bottom_left_x = 0
+                else:
+                    bottom_left_x = row_index + 1
 
-	def __str__(self):
-		the_string = ""
-		for row in self.state:
-			the_string += str(row) + "\n"
-		return the_string
+                bottom_y = cell_index
+                if row_index + 1 >= self.max_width:
+                    bottom_x = 0
+                else:
+                    bottom_x = row_index + 1
+
+                if cell_index + 1 >= self.max_height:
+                    bottom_right_y = 0
+                else:
+                    bottom_right_y = cell_index + 1
+
+                if row_index + 1 >= self.max_width:
+                    bottom_right_x = 0
+                else:
+                    bottom_right_x = row_index + 1
+
+                adjacent_cells = [  (top_left_x,top_left_y),
+                                    (top_x,top_y),
+                                    (top_right_x,top_right_y),
+                                    (left_side_x,left_side_y),
+                                    (right_side_x,right_side_y),
+                                    (bottom_left_x,bottom_left_y),
+                                    (bottom_x,bottom_y),
+                                    (bottom_right_x,bottom_right_y)]
+                #evaluate our next state based on rules
+
+                #get number of adjacent alive cells
+                our_state = int(self.state[row_index][cell_index])
+                alive_cells = 0
+
+                for cell in adjacent_cells:
+                    #if our_state is 1:
+                    #   print str(self.state[cell[0]][cell[1]])+" "+str( (cell[0],cell[1]) )
+                    #print adjacent_cells
+                    #print len(self.state)
+                    #print cell
+                    alive_cells += int(self.state[cell[0]][cell[1]])
+
+                
+                our_new_state = None
+                if our_state is 1:
+                    if alive_cells > 3:
+                        our_new_state = 0
+                    elif alive_cells < 2:
+                        our_new_state = 0
+                    elif alive_cells is 2 or alive_cells is 3:
+                        our_new_state = 1
+                    else:
+                        print "i cant even boolean logic"
+                else:
+                    if alive_cells is 3:
+                        our_new_state = 1
+                    else:
+                        our_new_state = 0
+                new_state[row_index][cell_index] = our_new_state
+                #if our_state is 1:
+                #   print alive_cells
+                #   print (row_index,cell_index)
+                    
+
+        #print "*****"
+        self.state = new_state
 
 
 
+        
 
+    def generate_state(self):
+        temp_state = []
 
+        for x in range(0,self.max_height):
+            temp_row = []
+            for y in range(0,self.max_width):
+                temp_row.append( random.randint(0,1) )
+            temp_state.append(temp_row)
+        return temp_state
+
+    def __str__(self):
+        the_string = ""
+        for row in self.state:
+            the_string += str(row) + "\n"
+        return the_string
 
 
 class App:
@@ -217,7 +217,7 @@ class App:
         self._running = True
         self._display_surf = None
         #TODO: make this work with a non-square grid
-        self.size = self.width, self.height = 900, 900
+        self.size = self.width, self.height = 800, 800
         self.bgcolor = 0, 0, 0
         self.DO_STEP_EVENT = pygame.USEREVENT+1
         self.IS_PAUSED = False
@@ -240,13 +240,13 @@ class App:
     def on_event(self, event):
         
         if event.type == self.DO_STEP_EVENT:
-	        self._display_surf.fill( self.bgcolor )
-        	self.theGrid.do_step()
-	        the_state = self.theGrid.state
-	        for row_index, row in enumerate(the_state):
-	        	for cell_index, cell in enumerate(row):
-	        		if int(the_state[row_index][cell_index]) is 1:
-	        			pygame.draw.circle(self._display_surf, (255,255,255), (row_index * self.CELL_SIZE + self.CELL_SIZE/2, cell_index * self.CELL_SIZE + self.CELL_SIZE/2), self.CELL_SIZE/2, self.CELL_SIZE/2/2)
+            self._display_surf.fill( self.bgcolor )
+            self.theGrid.do_step()
+            the_state = self.theGrid.state
+            for row_index, row in enumerate(the_state):
+                for cell_index, cell in enumerate(row):
+                    if int(the_state[row_index][cell_index]) is 1:
+                        pygame.draw.circle(self._display_surf, (255,255,255), (row_index * self.CELL_SIZE + self.CELL_SIZE/2, cell_index * self.CELL_SIZE + self.CELL_SIZE/2), self.CELL_SIZE/2, self.CELL_SIZE/2/2)
         
         if event.type == pygame.QUIT:
             self._running = False
@@ -256,19 +256,19 @@ class App:
             pass
 
 
-        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-            #print "mouse at (%d, %d)" % event.pos
-            #self._display_surf.fill( self.bgcolor )
-            #pygame.draw.circle(self._display_surf, (255,255,255), event.pos, 64, 0)
-            if self.IS_PAUSED is False:
-                self.IS_PAUSED = True
-                pygame.time.set_timer(self.DO_STEP_EVENT, 0)
-            else:
-                pygame.time.set_timer(self.DO_STEP_EVENT, self.STEP_SPEED)
-            #print "You released the left mouse button at (%d, %d)" % event.pos
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                if self.IS_PAUSED is False:
+                    self.IS_PAUSED = True
+                    pygame.time.set_timer(self.DO_STEP_EVENT, 0)
+                else:
+                    pygame.time.set_timer(self.DO_STEP_EVENT, self.STEP_SPEED)
+            elif event.button == 3:
+                self.theGrid.reset_state()
+            
             
     def on_loop(self):
-    	pass
+        pass
 
 
     
